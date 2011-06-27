88e7fb414ad3882eefe844ba620f64ddb6100d76
Fuji Goro <fuji.goro@dena.jp>
2011-06-27 15:04:16 +0900

    Add --debug-host and --debug-port options

--- bin/inspector.js
+++ bin/inspector.js
@@ -3,7 +3,10 @@
 var dserver = require('../lib/debug-server'),
     fs = require('fs'),
     path = require('path'),
-    options = {};
+    options = { // default settings
+      debugPort: 5858,
+      webPort:   8080,
+    };
 
 process.argv.forEach(function (arg) {
   var parts;
@@ -15,6 +18,12 @@ process.argv.forEach(function (arg) {
       case '--agent-port':
         options.webPort = parseInt(parts[1], 10);
         break;
+      case '--debug-port':
+        options.debugPort = parseInt(parts[1], 10);
+        break;
+      case '--debug-host':
+        options.debugHost = parts[1];
+        break;
       default:
         console.log('unknown option: ' + parts[0]);
         break;
@@ -23,7 +32,11 @@ process.argv.forEach(function (arg) {
     else if (parts[0] === '--help') {
       console.log('Usage: node [node_options] debug-agent.js [options]');
       console.log('Options:');
-      console.log('--web-port=[port]     port to host the inspector (default 8080)');
+      console.log('--web-port=[port]     port to host the inspector (default %s)', webPort);
+      console.log('--debug-port=[port]   port to the debuggee (default %s)',
+        options.debugPort);
+      console.log('--debug-host=[host]   host to the debuggee (default localhost)',
+        options.debugHost);
       process.exit();
     }
   }
@@ -44,10 +57,13 @@ fs.readFile(path.join(__dirname, '../config.json'), function(err, data) {
     }
   }
   if (!config.webPort) {
-    config.webPort = 8080;
+    config.webPort = options.webPort;
   }
   if (!config.debugPort) {
-    config.debugPort = 5858;
+    config.debugPort = options.debugPort;
+  }
+  if(!config.debugHost) {
+      config.debugHost = options.debugHost;
   }
   dserver.create(options, config).on('close', function () {
     console.log('session closed');
